046ba6a962581715636d5ae9e28ceade2eac68a9
Ryan Tucker <rtucker@gmail.com>
2010-06-06 23:35:21 -0400

    some munin magic for rgeoutages

---
+++ rgeoutages_munin.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEDIR=/var/www/hoopycat.com/html/rgeoutages
+
+if [ "$1" = "config" ]; then
+    cat <<EOM
+graph_title RG&E Power Outage Summary
+graph_args --base 1000 -l 0
+graph_vlabel outages
+graph_category Climate
+outages.draw AREA
+outages.label outages
+EOM
+
+elif [ "$1" = "autoconf" ]; then
+    if [ -f $BASEDIR/fetch_outages.sh ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
+
+else
+    outagecount=`cat $BASEDIR/outages_*.txt | wc -l`
+    echo "outages.data $outagecount"
+fi
+
